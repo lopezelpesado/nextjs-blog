22dfeb94a120824680813913e63204f3aed1b574
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-11-01 15:49:12 +0000

    prettier

--- components/date.js
+++ components/date.js
@@ -1,6 +1,8 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'do LLLL yyyy, GGGG')}</time>
+	const date = parseISO(dateString);
+	return (
+		<time dateTime={dateString}>{format(date, "do LLLL yyyy, GGGG")}</time>
+	);
 }
--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,13 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Custom404() {
-    return <h1>404! Where the hell do you think you're off to?! Get back to the{' '}
-        <Link href="/">
-            <a>home page</a>
-        </Link>!</h1>
-  }
+	return (
+		<h1>
+			404! Where the hell do you think you're off to?! Get back to the{" "}
+			<Link href='/'>
+				<a>home page</a>
+			</Link>
+			!
+		</h1>
+	);
+}

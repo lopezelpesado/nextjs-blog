df20e175a419b46447f96e37488bf5b13f403a07
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-11-01 15:24:44 +0000

    added profile links to home page

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Yo! I'm Alex Lopez or lopezelpesado! I'm learning to be a full stack web developer and, right now, I'm learning Next.js 👌 Check me out on GitHub and Twitter</p>
+        <p>Yo! I'm Alex Lopez or lopezelpesado! I'm learning to be a full stack web developer and, right now, I'm learning Next.js 👌 Check me out on <a href="https://github.com/lopezelpesado">GitHub</a> and <a href="https://twitter.com/lopezelpesado">Twitter 😎</a></p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This site was built following{' '}
+          <a href="https://nextjs.org/learn">this Next.js tutorial</a>.)
         </p>
       </section>
 	  <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
